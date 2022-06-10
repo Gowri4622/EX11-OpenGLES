2295b7a35b607019907e707ae345098da27d0fa8
Gowri <75235455+Gowri4622@users.noreply.github.com>
2022-06-10 09:12:11 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/opengl/MyGLActivity.java
@@ -0,0 +1,32 @@
+package com.example.opengl;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+public class MyGLActivity extends AppCompatActivity {
+
+    private GLSurfaceView glView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        glView = new GLSurfaceView(this);
+        glView.setRenderer(new MyGLRenderer(this));
+        this.setContentView(glView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        glView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        glView.onResume();
+    }
+}
